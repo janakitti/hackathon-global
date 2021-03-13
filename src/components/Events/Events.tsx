3967a83f9f41bc9dd432205a901bcbb541185bf5
Janakitti <janakittiratana@gmail.com>
2021-03-13 11:15:15 -0500

    Add Event types, move fetch to Event component

--- src/components/Events/Events.tsx
+++ src/components/Events/Events.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+import { TEndpointResponse } from "../../shared/EventTypes";
 import EventCard from "./EventCard/EventCard";
 
 const Events = () => {
+  useEffect(() => {
+    fetch(
+      "https://api.hackthenorth.com/v3/graphql?query={ events { id name event_type permission start_time end_time description speakers { name profile_pic } public_url private_url related_events } }"
+    )
+      .then((res) => res.json())
+      .then((data: TEndpointResponse) => console.log(data.data.events));
+  }, []);
+
   return <EventCard />;
 };
 
