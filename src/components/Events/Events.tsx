b9613009c26d051aa43071c1dd7b2085b3ca9e66
Janakitti <janakittiratana@gmail.com>
2021-03-13 11:45:07 -0500

    Format event cards

--- src/components/Events/Events.tsx
+++ src/components/Events/Events.tsx
@@ -1,17 +1,20 @@
-import { useEffect } from "react";
-import { TEndpointResponse } from "../../shared/EventTypes";
+import { useEffect, useState } from "react";
+import { TEndpointResponse, TEvent } from "../../shared/EventTypes";
 import EventCard from "./EventCard/EventCard";
 
 const Events = () => {
+  const [events, setEvents] = useState<TEvent[]>([]);
   useEffect(() => {
     fetch(
       "https://api.hackthenorth.com/v3/graphql?query={ events { id name event_type permission start_time end_time description speakers { name profile_pic } public_url private_url related_events } }"
     )
       .then((res) => res.json())
-      .then((data: TEndpointResponse) => console.log(data.data.events));
+      .then((data: TEndpointResponse) => setEvents(data.data.events));
   }, []);
 
-  return <EventCard />;
+  const eventCards = events.map((item: TEvent) => <EventCard event={item} />);
+
+  return <div id="events-page">{eventCards}</div>;
 };
 
 export default Events;
