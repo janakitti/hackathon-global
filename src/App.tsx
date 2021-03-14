732425a455d4e00c7b831beddd47a8e50278bd0b
Janakitti <janakittiratana@gmail.com>
2021-03-14 16:29:10 -0400

    Add login feature

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./styles/main.scss";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Events from "./components/Events/Events";
+import Login from "./components/Login/Login";
 import { AppProvider } from "./context/context";
 
 function App() {
@@ -10,26 +11,35 @@ function App() {
     <AppProvider>
       <div className="App">
         <Router>
-          <div id="router-container">
-            <div id="side-nav">
-              <Link to="/">
-                <div className="nav-link">Home</div>
-              </Link>
-              <Link to="/events">
-                <div className="nav-link">Events</div>
-              </Link>
-            </div>
-            <div id="page-window">
-              <Switch>
-                <Route path="/events">
-                  <Events />
-                </Route>
-                <Route path="/">
-                  <Home />
-                </Route>
-              </Switch>
-            </div>
-          </div>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/">
+              <Router>
+                <div id="router-container">
+                  <div id="side-nav">
+                    <Link to="/">
+                      <div className="nav-link">Home</div>
+                    </Link>
+                    <Link to="/events">
+                      <div className="nav-link">Events</div>
+                    </Link>
+                  </div>
+                  <div id="page-window">
+                    <Switch>
+                      <Route path="/events">
+                        <Events />
+                      </Route>
+                      <Route path="/">
+                        <Home />
+                      </Route>
+                    </Switch>
+                  </div>
+                </div>
+              </Router>
+            </Route>
+          </Switch>
         </Router>
       </div>
     </AppProvider>
---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,90 @@
+import { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import { AppContext } from "../../context/context";
+import { loginUser } from "../../context/actions/UserActions";
+
+const mockCredentials = {
+  username: "Mr. Goose",
+  email: "hack@hon.com",
+  password: "1234",
+  type: "hacker",
+};
+
+const Login = () => {
+  const history = useHistory();
+  const { dispatch } = useContext(AppContext);
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const [loginError, setLoginError] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (
+      user.email === mockCredentials.email &&
+      user.password === mockCredentials.password
+    ) {
+      setLoginError("");
+      dispatch(
+        loginUser({
+          username: mockCredentials.username,
+          email: mockCredentials.email,
+          type: "hacker",
+        })
+      );
+      history.push("/");
+    } else {
+      setLoginError("Invalid username or password");
+    }
+  };
+
+  return (
+    <div id="login__div--login-wrapper">
+      <div id="login__div--login-card">
+        <img
+          src="/logo_1.svg"
+          className="login__h1--logo"
+          width={150}
+          height={150}
+        />
+        <form onSubmit={handleSubmit}>
+          <input
+            className="login__input--text-input"
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={user.email}
+            onChange={handleChange}
+            required
+          ></input>
+          <input
+            className="login__input--text-input"
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={user.password}
+            onChange={handleChange}
+            required
+          ></input>
+          <Button
+            variant="primary"
+            type="submit"
+            id="login__span--login-button"
+          >
+            Log in
+          </Button>
+        </form>
+        <span id="login__span--login-error">{loginError}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
