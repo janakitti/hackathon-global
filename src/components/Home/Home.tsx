1db2ece3e4f3198b6ed996508df9e472776e260c
Janakitti <janakittiratana@gmail.com>
2021-03-15 22:38:25 -0400

    Add mock cards to Home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -10,10 +10,37 @@ const Home = () => {
       <div className="dashboard__div--inner-container">
         <div className="home__div--card">
           <h1 id="home__h1--welcome">
-            {user.type === "public" ? "Hey there," : "Welcome back,"}{" "}
+            {user.type === "public" ? "Hey there," : "Welcome back, "}
             {user.username} 👋
           </h1>
         </div>
+        <div id="home__card-container">
+          <div
+            className="home__card"
+            style={{ backgroundImage: `url("./resources.svg")` }}
+          >
+            <div className="home__card-body" id="home__card--resources">
+              Resources
+            </div>
+          </div>
+          <div
+            className="home__card"
+            style={{ backgroundImage: `url("./mentors.svg")` }}
+          >
+            <div className="home__card-body" id="home__card--mentors">
+              Mentors
+            </div>
+          </div>
+          <div
+            className="home__card"
+            style={{ backgroundImage: `url("./faq.svg")` }}
+          >
+            <div className="home__card-body" id="home__card--faq">
+              FAQ
+            </div>
+          </div>
+        </div>
+        <span id="home__note">Note: these cards are not yet implemented!</span>
       </div>
     </div>
   );
